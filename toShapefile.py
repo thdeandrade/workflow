ebb8a4cdd3806e97d04296d8feeafd85727c1b40
Thiago de Andrade Águas <thdeandrade@gmail.com>
2021-10-17 18:53:05 -0300

    Update toShapefile.py

--- toShapefile.py
+++ toShapefile.py
@@ -1,117 +1,107 @@
-# Informações de entrada
-p1 = r'.....\00_INIT\KML'
-p2 = r'.....\00_INIT\SHP_KML'
-p3 = r'.....\00_INIT\SHP_UTM'
-saida = r'.....\00_INIT'
-
+#input information 
+f = r'C:\firstSteps'
+folder1 = r'C:\firstSteps\00_ORIGINAL'
+ 
+arcpy.CreateFolder_management(r'C:/firstSteps/', '01_KML_TO_SHAPEFILE')
+folder2 = r'C:\firstSteps\01_KML_TO_SHAPEFILE'
+arcpy.CreateFolder_management(pf, '02_SHAPEFILE_UTM')
+folder3 = r'C:\firstSteps\02_SHAPEFILE_UTM'
 coord = arcpy.SpatialReference('SIRGAS 2000 UTM Zone 22S')
 
-empresa = 'NOME DA EMPRESA'
-lote = 'LOTE UTILIZADO'
-
-# Parte1
+#part1
 def toShapefile(input, output):
     """"
-    Identifica a estrutura do arquivo recebido e converte para o formato shapefile (.shp).
+    Identifies the file in kmz format and changes to shapefile (.shp).
+	:::input: Folder with initial files.
+	:::output: Folder with output files.
     """
 
     arcpy.env.workspace = input
-    l1 = arcpy.ListFiles(['*.kmz', '*.kml'])
+    files = arcpy.ListFiles('*.kmz')
 
-    for item in l1:
-        i = item.split('.')[0].replace(' - ', '_')
-        
+    for item in files:
+        i = item.split('.')[0]
         try:
-            arcpy.KMLToLayer_conversion(p1+'\\'+item,output,i)
+            arcpy.KMLToLayer_conversion(input+'\\'+item,output,i)
             
-            path = output + '\\' + i + '.gdb\\Placemarks\\Polygons'                 #Caso seja polígono
+            path = output + '\\' + i + '.gdb\\Placemarks\\Polygons'
             arcpy.FeatureToPolygon_management(path,output + '\\' + i +'.shp')
             print('Polygon: {}'.format(item))
 		
         except arcpy.ExecuteError:
-            path = output + '\\' + i + '.gdb\\Placemarks\\Polylines'                #Caso seja linha
+            path = output + '\\' + i + '.gdb\\Placemarks\\Polylines'
             arcpy.FeatureToPolygon_management(path,output + '\\' + i +'.shp')
             print('Polyline: {}'.format(item))
 
         except:
-            print('Error: {}'.format(item))                                         #Qualquer outro erro
+            print('Error: {}'.format(item))
             continue
-
-toShapefile(p1, p2)
-
-# Parte2
+			
+toShapefile(folder1, folder2)
+			
+#part2
 def toUTM(input, output, coordinate):
     """
-    Altera a projeção do arquivo shapefile (.shp) criado anteriormente (no caso, para UTM).
+    Change the coordinate system shapefile (.shp) file.
+	:::input: Folder with initial files.
+	:::output: Folder with output files.
+	:::coordinate: Pre-established coordinate system.
     """
     arcpy.env.workspace = input
-    l2 = arcpy.ListFiles('*.shp')
+    files = arcpy.ListFiles('*.shp')
 
-    for item in l2:
+    for item in files:
         i = item.split('.')[0]
         arcpy.management.Project(input + '\\' + item, output + '\\' + i + '_UTM.shp', coordinate)
 		
-toUTM(p2, p3, coord)
+toUTM(folder2, folder3, coord)
 
-# Parte 3
-def fillAttributes(input, company, batch):
+#part3
+def fillAttributes(input):
     """
-    Manipula a tabela de atributos (attribute table) para criar e preencher com informações inicias básicas e apaga o que não for necessário.
-    As colunas criadas são CÓDIGO DA FAZENDA, NOME DA FAZENDA, AREA (ha), NOME DO CLIENTE e LOTE.
+    Creates basic information in the attribute table and deletes what is not needed.
+	:::input: Folder with initial files.
     """
     arcpy.env.workspace = input
-    l3 = arcpy.ListFiles('*.shp')
+    files = arcpy.ListFiles('*.shp')
     
-    for i in l3:
+    for i in files:
         arcpy.AddField_management(i, 'CODE', 'TEXT')
         with arcpy.da.UpdateCursor(i, 'CODE') as rows:
             for row in rows:
                 if row[0] == '':
-                    row[0] = i.split('_')[0].replace(' ', '.')
+                    row[0] = i.split('_')[0]
                     rows.updateRow(row)
 		
-        arcpy.AddField_management(i, 'FARM', 'TEXT')
-        with arcpy.da.UpdateCursor(i, 'FARM') as rows:
+        arcpy.AddField_management(i, 'STATE', 'TEXT')
+        with arcpy.da.UpdateCursor(i, 'STATE') as rows:
             for row in rows:
                 if row[0] == '':
-                    row[0] = i.upper().split('_')[1]
+                    row[0] = i.split('_')[1]
                     rows.updateRow(row)
 
         arcpy.AddField_management(i, 'AREA', 'DOUBLE')
         arcpy.CalculateField_management(i, 'AREA', '!shape.Area@hectares!', "PYTHON", '')
 
-        arcpy.AddField_management(i, 'LOTE', 'TEXT')
-        with arcpy.da.UpdateCursor(i, 'LOTE') as rows:
-            for row in rows:
-                if row[0] == '':
-                    row[0] = batch
-                    rows.updateRow(row)
-				
-        arcpy.AddField_management(i, 'EMPRESA', 'TEXT')
-        with arcpy.da.UpdateCursor(i, 'EMPRESA') as rows:
-            for row in rows:
-                if row[0] == '':
-                    row[0] = company
-                    rows.updateRow(row)
-		
         remove = ['Id', 'FID_Polygo', 'Name', 'FolderPath', 'SymbolID', 'AltMode', 'Base', 'Clamped', 'Extruded', 'Snippet', 'PopupInfo', 'Shape_Leng', 'Shape_Area']
         arcpy.DeleteField_management(i, remove)
+		
+fillAttributes(folder3)
 
-fillAttributes(p3, empresa, lote)
-
-# Parte 4 - opcional
-def joinAll(input, output, company, batch):
+#part4
+def joinAll(input, output):
     """
-    Inclui todas as fazendas em um único arquivo (função opcional).
+    Include all states in a single shapefile (.shp) file.
+	:::input: Folder with initial files.
+	:::output: Folder with output files.
     """
-    arcpy.env.workspace = p3
-    l3 = arcpy.ListFiles('*.shp')
+    arcpy.env.workspace = input
+    files = arcpy.ListFiles('*.shp')
 	
     list = []
-    for i in l3:
+    for i in files:
         list.append(i)
 		
-    final_name = batch.split('.')
-    arcpy.Merge_management(list, output + '\\' + company + '_' + final_name[0] + '_' + final_name[1] + '.shp')
+    arcpy.Merge_management(list, output + '\\' + 'Brazilian_States.shp')
 
-joinAll(p3, saida, empresa, lote)
+joinAll(folder3, pf)
